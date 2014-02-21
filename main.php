b5493a1cf7eae4acce5c3bd2a3367b0c42de1a5b
Heesang Chae <theand@gmail.com>
2014-02-22 02:16:39 +0900

    Goutte test code, Monolog Integration

--- main.php
+++ main.php
@@ -4,13 +4,37 @@ require 'vendor/autoload.php';
 
 use Goutte\Client;
 
-$connector = PhpConsole\Connector::getInstance();
+use Monolog\Logger;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\ErrorHandler;
+
+use Carbon\Carbon;
+
+// create a log channel
+$log = new Logger('goutte');
+$log->pushHandler(new RotatingFileHandler('./logs/goutte.log', 0, Logger::WARNING));
+
+ErrorHandler::register($log);
+
 
 $client = new Client();
 
-$crawler = $client->request('GET',  'http://www.symfony-project.org/');
+$crawler = $client->request('GET',  'http://weather.media.daum.net/?pageId=103&metro=M100&area=11G00201');//제주 날씨
 
-$link = $crawler->filter(".list_submenu li");
+$crawler->filter("#Detail_wt .detail_dd .detail_left dl dd .dt2")->each( function(\Symfony\Component\DomCrawler\Crawler $node){
+    echo trim($node->text())."\n";
+});
 
-var_dump( $link);
+$cr = $crawler->filter("#Detail_wt .detail_dd .detail_left dl dd .dt2");
+echo trim($cr->text()) ."\n";
+
+$log->addWarning('----');
+$log->addWarning('Foo');
+$log->addError('Bar');
+$log->addInfo('this');
+$log->addInfo('that');
+
+throw new Exception('TEST');
+
+
 
